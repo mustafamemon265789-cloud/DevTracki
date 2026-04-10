d1dd308cb096e5fc71ef4e841ef88e52ff031c49
mustafamotiwala <mustafamemon265789@gmail.com>
2026-04-10 19:57:57 +0500

    Refactor FastAPI app initialization and add routers

--- APP/main.py
+++ APP/main.py
@@ -1,4 +1,30 @@
-from fastapi import FastAPI
-app = FastAPI(title= "DevTrack Api",
-              description = "Api for DevTrack, a project management tool" \
-              "For develepors and team to track their projects and tasks")
+from fastapi import FastAPI, Depends
+from app.database import create_db_and_tables
+from app.models.user import User, UserRead
+from app.models.project import Project
+from app.models.task import Task, Comment
+from app.routers import auth, projects, tasks
+from app.core.dependencies import get_current_user
+
+app = FastAPI(
+    title="DevTrack API",
+    description="API for DevTrack, a project management tool for developers and teams to track their projects and tasks",
+    version="1.0.0"
+)
+app.include_router(auth.router)
+app.include_router(projects.router)
+app.include_router(tasks.router)
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+    print("Tables Created Successfully")
+
+@app.get("/")
+def root():
+    return {"message": "Welcome To DevTrack API"}
+  
+@app.get("/users/me", response_model=UserRead, tags=["Users"])
+def get_me(current_user: User = Depends(get_current_user)):
+    """Get current authenticated user"""
+    return current_user
